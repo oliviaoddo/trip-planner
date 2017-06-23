026c445e91fdec38deb4b6020bd0befe82864edb
Olivia Oddo <olivia.oddo@gmail.com>
2017-06-23 15:00:03 -0400

    style

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 const route = require("express").Router();
 const models = require('../models');
 
-route.get("/", (req, res) => {
+route.get("/", (req, res, next) => {
     var outerScopeContainer = {};
     models.Hotel.findAll()
-    .then(function(hotels){
-
+    .then(function (dbHotels) {
+      outerScopeContainer.dbHotels = dbHotels;
+      return models.Restaurant.findAll();
     })
-  res.render("index.html");
+    .then(function (dbRestaurants) {
+      outerScopeContainer.dbRestaurants = dbRestaurants;
+      return models.Activity.findAll();
+    })
+    .then(function (dbActivities) {
+      res.render('index', {
+        templateHotels: outerScopeContainer.dbHotels,
+        templateRestaurants: outerScopeContainer.dbRestaurants,
+        templateActivities: dbActivities
+      });
+    })
+    .catch(next);
 });
 
 module.exports = route;
